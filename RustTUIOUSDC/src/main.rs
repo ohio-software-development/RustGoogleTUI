52bbea8ac02de804018ddd1a677da98d7acf0530
PrestonRembis <pr395420@ohio.edu>
2023-02-19 13:42:00 -0500

    Testing pushing to a specific branch

--- RustTUIOUSDC/src/main.rs
+++ RustTUIOUSDC/src/main.rs
@@ -1,14 +1,14 @@
 use cursive::views::{TextView, Dialog};
 use cursive::{Cursive, CursiveExt};
 
-
+//This is Preston's test main.rs
 
 fn main(){
     let mut siv = Cursive::new();
 
-    siv.add_layer(TextView::new("Welcome to the BlackBoard TUI!\nPress q to quit."));
+    siv.add_layer(TextView::new("Welcome to the BlackBoard TUI!\nPress p to quit."));
 
-    siv.add_global_callback('q', |s| s.quit());
+    siv.add_global_callback('p', |s| s.quit());
     
     siv.run();
 }
