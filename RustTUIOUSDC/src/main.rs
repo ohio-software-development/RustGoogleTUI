16d2cf97d52c4c19426c86deaca7f32ec1dcb56d
AlexBikowski20 <89482550+AlexBikowski20@users.noreply.github.com>
2023-02-19 13:47:29 -0500

    Team members has an OK button

--- RustTUIOUSDC/src/main.rs
+++ RustTUIOUSDC/src/main.rs
@@ -9,16 +9,21 @@ fn main() {
     // Creates a dialog with a single "Quit" button
     siv.add_layer(Dialog::around(TextView::new("Blackboard Rust TUI"))
         .title("Menu")
-        .button("Login", |s| s.quit())
-        .button("Team Members", |s| s.add_layer(
-            Dialog::around(TextView::new("Brady Phelps\nMichael Tan\nnPreston Rembis\nAlex Bikowski"))
-            .button("Back", |s| s.quit())
         
+        // button where login will occur
+        .button("Login", |s| s.quit())
+
+        // button to display team members
+        .button("Team Members", |s| s.add_layer(
+            Dialog::info("Brady Phelps\nMichael Tan\nnPreston Rembis\nAlex Bikowski")
         ))
+
+        // button to exit TUI
         .button("Quit", |s| s.quit())
                         
     );
 
+    // q will always quit the TUI
     siv.add_global_callback('q', |s| s.quit());
 
     // Starts the event loop.
