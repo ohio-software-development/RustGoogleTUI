2eb693beec7c9692741dcbe81831a4b5757722b8
Alex Bikowski <alexbikowski@pop-os.localdomain>
2023-02-19 13:39:36 -0500

    New Menu

--- RustTUIOUSDC/src/main.rs
+++ RustTUIOUSDC/src/main.rs
@@ -2,13 +2,26 @@ use cursive::views::{TextView, Dialog};
 use cursive::{Cursive, CursiveExt};
 
 
+fn main() {
+    // Creates the cursive root - required for every application.
+    let mut siv = cursive::default();
 
-fn main(){
-    let mut siv = Cursive::new();
-
-    siv.add_layer(TextView::new("Welcome to the BlackBoard TUI!\nPress q to quit."));
+    // Creates a dialog with a single "Quit" button
+    siv.add_layer(Dialog::around(TextView::new("Blackboard Rust TUI"))
+        .title("Menu")
+        .button("Login", |s| s.quit())
+        .button("Team Members", |s| s.add_layer(
+            Dialog::around(TextView::new("Brady Phelps\nMichael Tan\nnPreston Rembis\nAlex Bikowski"))
+            .button("Back", |s| s.quit())
+        
+        ))
+        .button("Quit", |s| s.quit())
+                        
+    );
 
     siv.add_global_callback('q', |s| s.quit());
-    
+
+    // Starts the event loop.
     siv.run();
 }
+
