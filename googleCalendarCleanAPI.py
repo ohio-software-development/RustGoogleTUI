568ff3a8063f4101bf6199712fb4d0bd6255b9e4
MonadoBarrage <chenkatti@tutanota.com>
2023-03-27 22:28:32 -0400

    Added functions inputDate and displayEvents so that they can be used in other functions

--- googleCalendarCleanAPI.py
+++ googleCalendarCleanAPI.py
@@ -19,6 +19,7 @@ def main():
     Prints the start and name of the next 10 events on the user's calendar.
     """
     creds = None
+    cont = True
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
@@ -35,39 +36,62 @@ def main():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+        
+    while(cont):
+        try:
+            print('Find All Events in a Date\n')
+            service = build('calendar', 'v3', credentials=creds)
+            displayEvents(service)
+            
 
-    try:
-        service = build('calendar', 'v3', credentials=creds)
+        except HttpError as error:
+            print('An error occurred: %s' % error)
 
-        fromDate = datetime.datetime(2023,3,24).isoformat() + 'Z'
-        toDate = datetime.datetime(2023,3,24,23,59,59).isoformat() + 'Z'
+# Asks user to input data for a specific date. Will try again if
+# input is invalid due to use of other symbols besides numbers or
+# date specified is not valid according to the Gregorian Calendar.
+# Is used by other functions.
+def inputDate():
+    cont = True
+    while(cont):
+        try:
+            year = input("Enter year: ")
+            month = input("Enter month (in numbers): ")
+            day = input("Enter day: ")
+            fromDate = datetime.datetime(int(year),int(month),int(day),0,0,0).isoformat() + 'Z'
+            toDate = datetime.datetime(int(year),int(month),int(day),23,59,59).isoformat() + 'Z'
+            cont = False      
+        except(ValueError):
+            print('Error! Date is not valid.')
+        except(TypeError):
+            print('Error! Invalid input.')
+        return fromDate,toDate
+
+
+# Displays all events that happened on a day as specified by the user.
+# Uses function inputDate to get date.
+def displayEvents(service):
+    fromDate,toDate = inputDate()
+        
         # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        eventNumber = input('Enter Number of Events to List: ')
-        while(not eventNumber.isdigit()):
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
             
-            eventNumber = input('Error! Enter Number of Events to List: ')
-            
-        print('Getting the upcoming ' + eventNumber + ' events')
-        events_result = service.events().list(calendarId='primary', timeMin=fromDate,timeMax=toDate,
-                                              maxResults=eventNumber, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+    print('Getting the upcoming events')
+    events_result = service.events().list(calendarId='primary', timeMin=fromDate,timeMax=toDate,
+                                            singleEvents=True,orderBy='startTime').execute()
+    events = events_result.get('items', [])
 
-        if not events:
-            print('No upcoming events found.')
-            return
+    if not events:
+        print('No upcoming events found.')
+        return
 
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-            pptest = event['start'].get('dateTime')
-            print(pptest)
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
+    # Prints the start and name of the next 10 events
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+        pptest = event['start'].get('dateTime')
+        print(pptest)
 
 
 if __name__ == '__main__':
-    main()
+    main()    
