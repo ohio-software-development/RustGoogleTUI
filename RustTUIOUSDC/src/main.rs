0a7ed1a8fe14c82399bf2458a8bf5297b7e17c4d
Alex Bikowski <ab641020@ohio.edu>
2023-04-23 15:52:35 -0400

    move to function

--- RustTUIOUSDC/src/main.rs
+++ RustTUIOUSDC/src/main.rs
@@ -229,87 +229,11 @@ fn go_to_next_email(siv: &mut Cursive, num: &i32) {
     }
 }
 
-fn calender_weekly(siv: &mut Cursive) {
+fn read_calender_string(siv: &mut Cursive, file: String) -> String {
 
-    siv.pop_layer();
-
-    let mut command = Command::new("python3").arg("../calendar_week.py").spawn().expect("error");
+    let mut command = Command::new("python3").arg(file).spawn().expect("error");
     command.wait().expect("error");
 
-    let mut file = std::fs::File::create("../file.txt").expect("error");
-
-    let mut display_string = "".to_string();
-
-    // Reads the information in calender.txt
-
-    let file_text = fs::read_to_string("../calendar.txt")
-        .expect("calendar.txt not read");
-
-    
-    // Text that is left to be searched 
-    let mut text_left = &file_text[0..file_text.len()];
-
-    // text that is left 
-    let mut bar_index_option = None;
-    let mut bar_index = 1;
-
-    let mut going = true; 
-    while (going) {
-
-        bar_index_option = text_left.find("|");
-
-        // get day
-        match bar_index_option {
-            Some(x) => bar_index = x,
-            None =>  siv.quit(),
-        }
-
-        let day = &text_left[0..bar_index];
-
-        text_left = &text_left[bar_index+1..];
-
-        bar_index_option = text_left.find("|");
-
-        match bar_index_option {
-            Some(x) => bar_index = x,
-            None =>  siv.quit(),
-        }
-
-        let title = &text_left[0..bar_index];
-
-        if (bar_index + 2 < text_left.len()) {
-
-            text_left = &text_left[bar_index+2..];
-        
-        } else {
-
-            going = false;
-
-        }
-
-        display_string = [display_string.to_string(), day.to_string(), title.to_string(), "\n".to_string()].join(" ");
-
-        file.write_all(title.as_bytes()).expect("error");
-        file.write_all(b"\n").expect("error");
-
-    }
-
-
-    let events = Dialog::new()
-    .content(TextView::new(display_string))
-    .button("Back", calendar);
-    
-    siv.add_layer(events);
-
-}
-
-
-fn calender_date(siv: &mut Cursive) {
-
-    siv.pop_layer();
-
-    let mut command = Command::new("python3").arg("../calendar_date.py").spawn().expect("error");
-    command.wait().expect("error");
 
     let mut file = std::fs::File::create("file.txt").expect("error");
 
@@ -369,12 +293,37 @@ fn calender_date(siv: &mut Cursive) {
 
     }
 
+    return display_string;
+
+}
+
+fn calender_weekly(siv: &mut Cursive) {
+
+    siv.pop_layer();
+
+    let display_string = read_calender_string(siv, "../calendar_week.py".to_string());
 
     let events = Dialog::new()
     .content(TextView::new(display_string))
     .button("Back", calendar);
     
     siv.add_layer(events);
+
+}
+
+
+fn calender_date(siv: &mut Cursive) {
+
+    siv.pop_layer();
+
+    let display_string = read_calender_string(siv, "../calendar_date.py".to_string());
+
+    let events = Dialog::new()
+    .content(TextView::new(display_string))
+    .button("Back", calendar);
+    
+    siv.add_layer(events);
+
 
 }
 
